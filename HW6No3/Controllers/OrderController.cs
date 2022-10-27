f38efa9627afd5543d1b2f081c8f42656bf2f35a
Marco <mclarenmarco998@gmail.com>
2022-10-27 17:28:40 +0200

    Add project files.

---
+++ HW6No3/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using HW6No3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6No3.Controllers
+{
+    public class OrderController : Controller
+    {
+        public BikeStoresEntities db = new BikeStoresEntities();
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View(db.orders.ToList());
+        }
+    }
+}
